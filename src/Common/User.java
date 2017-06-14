33fd10356c141eda7a5df009ab78e3242245d549
Rémi B <remibouguermouh@gmail.com>
2017-06-14 11:40:55 +0200

    Add Rest Auth

--- src/Common/User.java
+++ src/Common/User.java
@@ -1,9 +1,11 @@
 package Common;
 
+import java.io.Serializable;
+
 /**
  * Created by bouguerr on 31/05/2017.
  */
-public class User {
+public class User implements Serializable {
     private String login;
     private String password;
     private boolean isLogged;
--- src/chat/T2.java
+++ src/chat/T2.java
@@ -1,27 +1,65 @@
 package chat;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 import ChatAuth.ServiceAuth;
 import Common.User;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 
+
+@Path("/api")
 public class T2
 {
-	public static void main(String [] args) throws Exception 
+
+
+	public static void main(String [] args) throws Exception
 	{
-		System.out.println("On client");
+
+	}
+
+
+	@Path("/login")
+	@GET
+	@Produces("application/xml")
+	public String convertCtoF()
+	{
+		// lien d'acces
+		// http://localhost:8080/TempaceWS/tempace/ServiceClientAuth/
+		Double fahrenheit;
+		Double celsius = 36.8;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+
+
+	@Path("/login")
+	@POST
+	@Produces("application/json")
+	public Response login(@FormParam("login") String login, @FormParam("password") String password) throws RemoteException, NotBoundException
+	{
+
+		ServiceAuth authService;
 		Registry registry = LocateRegistry.getRegistry("127.0.0.1",2000);
-		ServiceAuth service;
-		service = (ServiceAuth) registry.lookup("Auth");
+		authService = (ServiceAuth) registry.lookup("Auth");
 
+		if(login.length() > 0 && password.length() > 0) {
+			User u = authService.authentification(login, password);
+			if(u != null) {
 
-		System.out.println("User auth 1");
-		User user = service.authentification("toto", "toto");
-		System.out.println("Create user");
-		service.newUser("toto", "toto");
-		System.out.println("User auth 2");
-		user = service.authentification("toto", "toto");
+				return Response.ok().build();
+			} else {
+				return Response.status(Response.Status.BAD_REQUEST).build();
+			}
+		} else {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+
 	}
 }
